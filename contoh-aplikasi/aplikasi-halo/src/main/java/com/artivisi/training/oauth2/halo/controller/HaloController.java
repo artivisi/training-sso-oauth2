f0937062f77bac68d0fac503f4746e90476d20f0
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-05 14:53:43 +0700

    menerima request parameter

--- contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/controller/HaloController.java
+++ contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/controller/HaloController.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HaloController {
     
     @RequestMapping("/halo")
-    public void halo(Model m){
+    public void halo(Model m, @RequestParam(required = false) String nama){
         m.addAttribute("waktu", new Date());
+        if(nama != null && !nama.isEmpty()){
+            m.addAttribute("pesan", "Halo "+nama);
+        }
     }
     
     @RequestMapping("/api/halo")
