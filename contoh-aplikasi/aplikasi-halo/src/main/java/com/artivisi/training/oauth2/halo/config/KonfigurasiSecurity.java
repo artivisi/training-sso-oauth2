ecf70bf9fadc0f315f7208d37c6f32d5159ba0d4
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-05 15:22:50 +0700

    custom login page

--- contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/config/KonfigurasiSecurity.java
+++ contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/config/KonfigurasiSecurity.java
@@ -3,6 +3,7 @@ package com.artivisi.training.oauth2.halo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -13,5 +14,14 @@ public class KonfigurasiSecurity  extends WebSecurityConfigurerAdapter {
                 .withUser("endy")
                     .password("123")
                     .roles("USER"); 
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll();
     }
 }
---
+++ contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/config/KonfigurasiWeb.java
@@ -0,0 +1,13 @@
+package com.artivisi.training.oauth2.halo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class KonfigurasiWeb extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
