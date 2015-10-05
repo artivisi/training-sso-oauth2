a5144ff9af276520efdcff079bd2ce68a0137d5f
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-05 14:46:36 +0700

    contoh RESTful webservice dengan Spring Boot

--- contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/controller/HaloController.java
+++ contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/controller/HaloController.java
@@ -1,9 +1,13 @@
 package com.artivisi.training.oauth2.halo.controller;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HaloController {
@@ -11,5 +15,21 @@ public class HaloController {
     @RequestMapping("/halo")
     public void halo(Model m){
         m.addAttribute("waktu", new Date());
+    }
+    
+    @RequestMapping("/api/halo")
+    @ResponseBody
+    public Map<String, Object> haloApi(@RequestBody(required = false) Map<String, String> input){
+        Map<String, Object> data = new HashMap<>();
+        data.put("waktu", new Date());
+        
+        if(input != null){
+            String nama = input.get("nama");
+            if(nama != null && !nama.isEmpty()){
+                data.put("pesan", "Halo "+nama);
+            }
+        }
+        
+        return data;
     }
 }
