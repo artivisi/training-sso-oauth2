fcf7e1512749227aea0682207d60f6ed1d91c010
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-06 09:59:41 +0700

    template project dari start.spring.io

---
+++ contoh-aplikasi/oauth-server/src/main/java/com/artivisi/training/oauth2/AuthorizationServerApplication.java
@@ -0,0 +1,12 @@
+package com.artivisi.training.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
