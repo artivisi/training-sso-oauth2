18fc10fde48567bd3cae3f7ce6b98ba0638133da
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-06 13:15:50 +0700

    implement resource server

---
+++ contoh-aplikasi/aplikasi-halo/src/main/java/com/artivisi/training/oauth2/halo/config/KonfigurasiResourceServer.java
@@ -0,0 +1,39 @@
+package com.artivisi.training.oauth2.halo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+
+@Configuration
+public class KonfigurasiResourceServer {
+    private static final String RESOURCE_ID = "belajar";
+    
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends
+            ResourceServerConfigurerAdapter {
+        
+        
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            RemoteTokenServices tokenService = new RemoteTokenServices();
+            tokenService.setClientId("clientauthcode");
+            tokenService.setClientSecret("123456");
+            tokenService.setCheckTokenEndpointUrl("http://localhost:10000/oauth/check_token");
+            
+            resources
+                    .resourceId(RESOURCE_ID)
+                    .tokenServices(tokenService);
+        }
+        
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/api/halo").hasRole("OPERATOR");
+        }
+    }
+}
--- contoh-aplikasi/oauth-server/src/main/java/com/artivisi/training/oauth2/authserver/config/KonfigurasiAuthorizationServer.java
+++ contoh-aplikasi/oauth-server/src/main/java/com/artivisi/training/oauth2/authserver/config/KonfigurasiAuthorizationServer.java
@@ -2,6 +2,7 @@ package com.artivisi.training.oauth2.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 public class KonfigurasiAuthorizationServer {
@@ -45,6 +47,7 @@ public class KonfigurasiAuthorizationServer {
                     .withClient("clientauthcode")
                         .secret("123456")
                         .authorizedGrantTypes("authorization_code", "refresh_token")
+                        .authorities("CLIENT")
                         .scopes("read", "write")
                         .resourceIds(RESOURCE_ID)
                     .and()
