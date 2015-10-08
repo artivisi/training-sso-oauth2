e8d28c8e2057a84e6b6cf3456b05221a68415337
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-08 10:15:52 +0700

    starter project spring

---
+++ contoh-aplikasi/aplikasi-client-oauth2-implicit/src/main/java/com/artivisi/training/oauth2/AplikasiClientOauth2ImplicitApplication.java
@@ -0,0 +1,12 @@
+package com.artivisi.training.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiClientOauth2ImplicitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplikasiClientOauth2ImplicitApplication.class, args);
+    }
+}
