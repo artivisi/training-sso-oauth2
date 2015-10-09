759cb3d9a4a1a1358d3525194f0b9dcab74ce964
Endy Muhardin <endy.muhardin@gmail.com>
2015-10-09 14:18:53 +0700

    starter android project

---
+++ contoh-aplikasi/AndroidOauthClient/app/src/main/java/com/artivisi/training/androidoauthclient/LoginOauthActivity.java
@@ -0,0 +1,35 @@
+package com.artivisi.training.androidoauthclient;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class LoginOauthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_oauth);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void login(View view){
+        Toast.makeText(this, "Login Dulu",Toast.LENGTH_SHORT)
+        .show();
+    }
+
+}
